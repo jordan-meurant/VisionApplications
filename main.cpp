8d652a5175ab0b800b2249725713ee7ba8bcd77f
ArthurDetrembleur <arthur.detrembleur@student.hepl.be>
2022-09-25 16:08:34 +0200

    balanes

--- main.cpp
+++ main.cpp
@@ -3,20 +3,21 @@
 
 #define CVUI_IMPLEMENTATION
 #include "cvui.h"
+#define IMAGE_PATH(filename) std::string("D:\\Users\\mb624\\Documents\\GitHub\\VisionApplications\\ImagesEtape5\\") + filename
 
 #define WINDOW_NAME	"CVUI Test"
 
 using namespace cv;
 using namespace cvui;
-void tools(){
+void tools() {
 
-    Mat tools = cv::imread("/Users/jordanmeurant/CLionProjects/Vision1/ImagesEtape5/tools.png");
-    Mat frame, out,opening, th;
+    Mat tools = cv::imread(IMAGE_PATH("tools.png"));
+    Mat frame, out, opening, th;
 
-    cvtColor(tools,frame, COLOR_BGR2GRAY);
+    cvtColor(tools, frame, COLOR_BGR2GRAY);
     cv::imshow("Image tools", tools);
 
-    morphologyEx(frame,opening, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(21,21)));
+    morphologyEx(frame, opening, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(21, 21)));
     subtract(frame, opening, frame);
     cv::imshow("Image after opening", frame);
 
@@ -26,49 +27,83 @@ void tools(){
 
 }
 
-
-int main(int argc, const char *argv[])
+Mat imreconstruct(Mat& image, Mat& mask, int radius = 3)
 {
-    tools();
-//    Mat lena = cv::imread("/Users/jordanmeurant/CLionProjects/Vision1/ImagesEtape5/vaisseaux.jpg");
-//    Mat frame = lena.clone();
-//
-//
-//    int scale_percent = 100;
-//    int width = int(frame.rows * scale_percent / 100);
-//    int height = int(frame.cols * scale_percent / 100);
-//    Size dim = Size(width, height);
-//
-//    // resize image
-//    Mat resizedImage;
-//    resize(frame, resizedImage, dim, INTER_LINEAR);
-//
-//
-//    int low_threshold = 50, high_threshold = 150;
-//    bool use_canny = false;
-//
-//    namedWindow(WINDOW_NAME);
-//    init(WINDOW_NAME);
-//
-//    while (true) {
-//        if (use_canny) {
-//            cvtColor(resizedImage, frame, COLOR_BGR2GRAY);
-//            Canny(frame, frame, low_threshold, high_threshold, 3);
-//        } else {
-//            resizedImage.copyTo(frame);
-//        }
-//
-//        window(frame, 10, 50, 180 , 180 , "Settings");
-//        checkbox(frame, 15, 80, "Use Canny Edge", &use_canny);
-//        cvui::trackbar(frame, 15, 110, 165, &low_threshold, 5, 150);
-//        cvui::trackbar(frame, 15, 180, 165, &high_threshold, 80, 300);
-//
-//        update();
-//        cv::imshow(WINDOW_NAME, frame);
-//
-//        if (cv::waitKey(30) == 27) {
-//            break;
-//        }
-//    }
-//    return 0;
+    int counter = 0;
+    while (1)
+    {
+        Mat mat;
+        morphologyEx(image, mat, MORPH_DILATE, getStructuringElement(MORPH_RECT, Size(radius, radius)));
+        bitwise_and(mat, mask, mat);
+        if (countNonZero(image) == countNonZero(mat))
+            return mat;
+        image = mat;
+    }
+}
+
+void balanes()
+{
+    Mat frame, seuillage, erosion;
+    Mat balanes = cv::imread(IMAGE_PATH("balanes.png"));
+    cvtColor(balanes, frame, COLOR_BGR2GRAY);
+    cvtColor(balanes, balanes, COLOR_BGR2GRAY);
+    threshold(frame, seuillage, 0, 255, THRESH_BINARY + THRESH_OTSU);
+    morphologyEx(seuillage, erosion, MORPH_ERODE, getStructuringElement(MORPH_RECT, Size(11, 11)));
+    const Mat& rec = imreconstruct(erosion, seuillage);
+    Mat origin;
+    bitwise_and(rec, balanes, origin);
+    cv::imshow("GRANDE", origin);
+    Mat petitesBalanes;
+    subtract(seuillage, rec, petitesBalanes);
+    morphologyEx(petitesBalanes, petitesBalanes, MORPH_ERODE, getStructuringElement(MORPH_RECT, Size(5, 5)));
+    Mat rec2 = imreconstruct(petitesBalanes, seuillage);
+    bitwise_and(rec2, balanes, origin);
+    cv::imshow("PETITES", origin);
+    waitKey();
+}
+
+int main(int argc, const char* argv[])
+{
+    balanes();
+    //    Mat lena = cv::imread("/Users/jordanmeurant/CLionProjects/Vision1/ImagesEtape5/vaisseaux.jpg");
+    //    Mat frame = lena.clone();
+    //
+    //
+    //    int scale_percent = 100;
+    //    int width = int(frame.rows * scale_percent / 100);
+    //    int height = int(frame.cols * scale_percent / 100);
+    //    Size dim = Size(width, height);
+    //
+    //    // resize image
+    //    Mat resizedImage;
+    //    resize(frame, resizedImage, dim, INTER_LINEAR);
+    //
+    //
+    //    int low_threshold = 50, high_threshold = 150;
+    //    bool use_canny = false;
+    //
+    //    namedWindow(WINDOW_NAME);
+    //    init(WINDOW_NAME);
+    //
+    //    while (true) {
+    //        if (use_canny) {
+    //            cvtColor(resizedImage, frame, COLOR_BGR2GRAY);
+    //            Canny(frame, frame, low_threshold, high_threshold, 3);
+    //        } else {
+    //            resizedImage.copyTo(frame);
+    //        }
+    //
+    //        window(frame, 10, 50, 180 , 180 , "Settings");
+    //        checkbox(frame, 15, 80, "Use Canny Edge", &use_canny);
+    //        cvui::trackbar(frame, 15, 110, 165, &low_threshold, 5, 150);
+    //        cvui::trackbar(frame, 15, 180, 165, &high_threshold, 80, 300);
+    //
+    //        update();
+    //        cv::imshow(WINDOW_NAME, frame);
+    //
+    //        if (cv::waitKey(30) == 27) {
+    //            break;
+    //        }
+    //    }
+    //    return 0;
 }
